fb77b8ec6d1d1f52f9e88bd1a389b515fa25e465
kenobi <kenobi@kenobia.es>
2023-01-18 15:29:50 +0000

    Changed vote options to cats and dogs

--- vote/app.py
+++ vote/app.py
@@ -5,8 +5,8 @@ import socket
 import random
 import json
 
-option_a = os.getenv('OPTION_A', "Emacs")
-option_b = os.getenv('OPTION_B', "Vi")
+option_a = os.getenv('OPTION_A', "cats")
+option_b = os.getenv('OPTION_B', "dogs")
 hostname = socket.gethostname()
 version = 'v1'
 
